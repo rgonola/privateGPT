ba8ff5c3773b9334332dad0b7eddffbc6b57cd7f
rgonola <gonolarajesh@gmail.com>
2023-08-16 23:52:41 -0400

    updates

--- privateGPT.py
+++ privateGPT.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from tkinter import *
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
@@ -8,6 +9,15 @@ from langchain.llms import GPT4All, LlamaCpp
 import os
 import argparse
 import time
+
+
+
+# def send_message():
+#     message = entry.get()
+#     if message:
+#         main(message)
+        #chat_box.insert(END, "You: " + message + "\n", "sender")
+        #entry.delete(0, END)
 
 load_dotenv()
 
@@ -22,7 +32,12 @@ target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 
 from constants import CHROMA_SETTINGS
 
-def main():
+
+def send_message():
+
+    message = e.get()
+    txt.insert(END, "You: " + message + "\n", "sender")
+    e.delete(0, END)
     # Parse the command line arguments
     args = parse_arguments()
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
@@ -41,30 +56,34 @@ def main():
             raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
         
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
+   
+   
     # Interactive questions and answers
-    while True:
-        query = input("\nEnter a query: ")
-        if query == "exit":
-            break
-        if query.strip() == "":
-            continue
+    #while True:
+       # query = input("\nEnter a query: ")
+       # if query == "exit":
+       #     break
+       # if query.strip() == "":
+       #     continue
 
-        # Get the answer from the chain
+        #Get the answer from the chain
+    #message = e.get()
+    if message:    
         start = time.time()
-        res = qa(query)
+        res = qa(message)
         answer, docs = res['result'], [] if args.hide_source else res['source_documents']
         end = time.time()
 
-        # Print the result
-        print("\n\n> Question:")
-        print(query)
-        print(f"\n> Answer (took {round(end - start, 2)} s.):")
-        print(answer)
-
-        # Print the relevant sources used for the answer
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
+        # # Print the result
+        # print("\n\n> Question:")
+        # print(message)
+        # print(f"\n> Answer (took {round(end - start, 2)} s.):")
+        # print(answer)
+        txt.insert(END, "Bot: " + answer + "\n", "receiver")
+        # # Print the relevant sources used for the answer
+        # for document in docs:
+        #     print("\n> " + document.metadata["source"] + ":")
+        #     print(document.page_content)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
@@ -78,6 +97,44 @@ def parse_arguments():
 
     return parser.parse_args()
 
+def clear_chat():
+    txt.delete(1.0, END)
+#if __name__ == "__main__":
+#    main()
 
-if __name__ == "__main__":
-    main()
+
+
+# GUI
+root = Tk()
+root.title("Chatbot")
+
+BG_GRAY = "#ABB2B9"
+BG_COLOR = "#17202A"
+TEXT_COLOR = "#EAECEE"
+
+FONT = "Helvetica 14"
+FONT_BOLD = "Helvetica 13 bold"
+
+# Send function
+
+lable1 = Label(root, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome", font=FONT_BOLD, pady=10, width=20, height=1).grid(
+	row=0)
+
+txt = Text(root, bg=BG_COLOR,  fg=TEXT_COLOR,font=FONT, width=100)
+txt.grid(row=1, column=0, columnspan=2,padx=5,pady=2)
+#txt.tag_configure("sender", foreground="blue")
+#txt.tag_configure("receiver", foreground="red")
+
+scrollbar = Scrollbar(txt)
+scrollbar.place(relheight=1, relx=0.974)
+
+e = Entry(root, bg="#2C3E50", fg=TEXT_COLOR, font=FONT, width=80)
+e.grid(row=2, column=0)
+
+send = Button(root, text="Send", font=FONT_BOLD, bg=BG_GRAY,
+			command=send_message).grid(row=2, column=1)
+
+clear_button = Button(root, text="Clear", font=FONT_BOLD, bg=BG_GRAY,
+			command=clear_chat).grid(row=2, column=2)
+
+root.mainloop()
